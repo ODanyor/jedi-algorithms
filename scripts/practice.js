77d2b15e8ce691d7156d996cec59a46c049ee291
Doniyor Otamurodov <doniyorgit@gmail.com>
2020-12-17 19:14:34 +0500

    'update'

--- scripts/practice.js
+++ scripts/practice.js
@@ -2,46 +2,88 @@ function main () {
   console.log("<<<=== Practice Time ===>>>");
 
   const numbers = [19, 12, 5, 2, 0, -1, -5, -14];
-  // TASK: sort numbers via Bubble Sort
+
   function bubbleSort (array) {
-    const resultArray = array.slice();
+    const sortedArray = array.slice();
 
-    for (let outer = 0; outer < array.length; outer++) {
-      let outerElement = resultArray[outer];
+    for (let outer = 0; outer < sortedArray.length; outer++) {
+      let outerElement = sortedArray[outer];
 
-      for (let inner = outer + 1; inner < resultArray.length; inner++) {
-        let innerElement = resultArray[inner];
+      for (let inner = outer + 1; inner < sortedArray.length; inner++) {
+        let innerElement = sortedArray[inner];
 
         if (outerElement > innerElement) {
-          resultArray[outer] = innerElement;
-          resultArray[inner] = outerElement;
+          sortedArray[outer] = innerElement;
+          sortedArray[inner] = outerElement;
 
-          outerElement = resultArray[outer];
-          innerElement = resultArray[inner];
-        }
-      }
-    }
-
-    return resultArray;
-  }
-  // measurePerfomance(() => bubbleSort(numbers))
-
-  function bubbleSort2 (array) {
-    const sortedArray = [...array];
-
-    for (let outer = 0; outer < array.length; outer++) {
-      for (let inner = outer + 1; inner < array.length; inner++) {
-        if (array[outer] > array[inner]) {
-          let outerElement = array[outer];
-          array[outer] = array[inner];
-          array[inner] = outerElement;
+          outerElement = sortedArray[outer];
+          innerElement = sortedArray[inner];
         }
       }
     }
 
     return sortedArray;
+  } // O(n)
+  // measurePerfomance(() => bubbleSort(numbers));
+
+  function quickSort (array) {
+    const copiedArray = array.slice();
+
+    if (copiedArray.length <= 1) return copiedArray;
+
+    const pElement = copiedArray.shift(); // pivot element
+    const boxA = []; // box for items smaller than pElement
+    const boxB = [pElement];
+    const boxC = []; // box for items bigger than pElement
+
+    while (copiedArray.length) {
+      const cElement = copiedArray.shift(); // current element
+
+      if (pElement === cElement) boxB.push(cElement);
+      else if (pElement > cElement) boxA.push(cElement);
+      else boxC.push(cElement);
+    }
+
+    const sortedBoxA = quickSort(boxA);
+    const sortedBoxB = quickSort(boxB);
+    const sortedBoxC = quickSort(boxC);
+
+    return sortedBoxA.concat(sortedBoxB, sortedBoxC);
+  } // The Master Theorem
+  // measurePerfomance(() => quickSort(numbers));
+
+  function mergeSort (array) {
+    if (array.length === 1) return array;
+
+    if (array.length === 2) return array[0] > array[1] ? [array[1], array[0]] : array;
+
+    const m = Math.floor(array.length / 2); // middle index
+    const lSortedArray = mergeSort(array.slice(0, m)); // left sorted array
+    const rSortedArray = mergeSort(array.slice(m)); // right sorted array
+
+    const mArray = []; // merged array
+    let lIndex = 0; // left index
+    let rIndex = 0; // right index
+
+    while (
+        lIndex < lSortedArray.length ||
+        rIndex < rSortedArray.length
+      ) {
+      if (
+          lIndex >= lSortedArray.length ||
+          lSortedArray[lIndex] > rSortedArray[rIndex]
+        ) {
+        mArray.push(rSortedArray[rIndex]);
+        rIndex++;
+      } else {
+        mArray.push(lSortedArray[lIndex]);
+        lIndex++;
+      }
+    }
+
+    return mArray;
   }
-    measurePerfomance(() => bubbleSort2(numbers), 0);
+  measurePerfomance(() => mergeSort(numbers));
 }
 
 window.addEventListener("load", main);
